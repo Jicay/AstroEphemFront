940a3da47851db8f0363f1fa1650633c085c11bb
jcsabourin <jeanclement.sabourin@gmail.com>
2018-11-06 00:29:02 +0100

    Second

---
+++ src/components/views/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class Home extends Component {
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div id="home">
+                This is the home page.
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,63 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from "./components/views/home";
+import Contact from "./components/views/contact";
 
-import './myStyles.scss';
-
-class App extends React.Component {
-    state = {
-        CaptainKirkBio: {},
-        Foo: null, // Foo is out component
-    };
-
-    componentDidMount() {
-        this.onGetKirkBio();
-        import(/* webpackChunkName: 'Foo' */ './Foo').then(Foo => {
-            this.setState({ Foo: Foo.default });
-        });
-    }
-
-    onGetKirkBio = async () => {
-        try {
-            const result = await fetch('http://stapi.co/api/v1/rest/character/search', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: {
-                    title: 'James T. Kirk',
-                    name: 'James T. Kirk',
-                },
-            });
-            const resultJSON = await result.json();
-            const character = resultJSON.characters[0];
-            this.setState({ CaptainKirkBio: character });
-        } catch (error) {
-            console.log('error', error);
-        }
-    };
-
-    render() {
-        const { CaptainKirkBio, Foo } = this.state;
-        return (
-            <div className="app">
-                <img alt="header" src="/dist/images/header.jpg" className="app-header" />
-                <p>
-                    We are a most promising species, Mr. Spock, as predators go. Did you know that? I
-                    frequently have my doubts. I dont. Not any more. And maybe in a thousand years or so will
-                    be able to prove it.
-                </p>
-                <p>- Captain Kirk</p>
-                <section>
-                    {Object.values(CaptainKirkBio).length === 0 ? (
-                        <p>Loading User Information</p>
-                    ) : (
-                        <p style={{ wordBreak: 'break-all' }}>{JSON.stringify(CaptainKirkBio)}</p>
-                    )}
-                </section>
-                {Foo ? <Foo /> : <p>Foo is loading</p>}
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDom.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/*' component={Home} />
+        </Switch>
+    </BrowserRouter>,
+    document.querySelector('#app')
+);
