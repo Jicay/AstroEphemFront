fd283f69cf33cd2b33d8aab7ea6ead4e39c18c11
jcsabourin <jeanclement.sabourin@gmail.com>
2018-11-06 00:34:08 +0100

    Third

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./components/views/home";
 import Contact from "./components/views/contact";
 
-ReactDom.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path='/contact' component={Contact} />
-            <Route path='/*' component={Home} />
-        </Switch>
-    </BrowserRouter>,
-    document.querySelector('#app')
-);
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route path='/*' component={Home} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
