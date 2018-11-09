d70284de19bd924184562b83083d7502e0031852
Jicay <jeanclement.sabourin@gmail.com>
2018-11-09 11:06:40 +0100

    Ajout bootstrap

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
-import { Button } from 'reactstrap';
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+} from 'reactstrap';
 export default class Home extends Component {
     componentDidMount() {
     }
     render() {
         return (
-            <div id="home">
-                This is the home page.
-                <Button>Click !</Button>
-            </div>
+            <Container className="home">
+                <h2>Sign In</h2>
+                <Form className="form">
+                    <Col>
+                        <FormGroup>
+                            <Label>Email</Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                id="exampleEmail"
+                                placeholder="myemail@email.com"
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                            <Label for="examplePassword">Password</Label>
+                            <Input
+                                type="password"
+                                name="password"
+                                id="examplePassword"
+                                placeholder="********"
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Button>Submit</Button>
+                </Form>
+            </Container>
         );
     }
 }
