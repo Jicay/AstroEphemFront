7390dc5c26544974b945ebb0e3d9eafae5e718f6
Jicay <jeanclement.sabourin@gmail.com>
2018-11-19 14:12:53 +0100

    Ajout style et gradient

--- src/components/views/day.js
+++ src/components/views/day.js
@@ -4,6 +4,7 @@ import {Col, Container, Row} from 'reactstrap';
 import React from "react";
 
 
+import '../../days.scss';
 
 export default class Day extends Component {
     static propTypes = {
@@ -20,16 +21,44 @@ export default class Day extends Component {
             setting_astronomical: string
         })
     }
+
     render() {
         const { date, sun } = this.props.day;
-        console.log(this.props);
+        const weekDay = new Intl.DateTimeFormat('fr-FR', {
+            weekday: "long"
+        }).format(new Date(date));
+
+        const day = new Intl.DateTimeFormat('fr-FR', {
+            day: "2-digit"
+        }).format(new Date(date));
+
+
+        const year = new Intl.DateTimeFormat('fr-FR', {
+            month: "short",
+            year: "numeric"
+        }).format(new Date(date));
+
+        const rising_astronomical = new Date(sun.rising_astronomical)
+
+        const nbSecond = 24*60*60;
+        var secs = rising_astronomical.getSeconds() + (60 * rising_astronomical.getMinutes()) + (60 * 60 * rising_astronomical.getHours());
+        const percent = 100 * secs / nbSecond;
+
+        const style = "linear-gradient(to right, #000000 " + secs / nbSecond + "%, #2B5695)"
+
+        console.log(style);
+
+        console.log(weekDay, day, year);
         return (
             <Container className="day">
                 <Row>
-                    <Col xs="3">
-                        {date}
+                    <Col xs="2" className="day_date">
+                        <span>{weekDay}</span>
+                        {day}
+                        <span>{year}</span>
                     </Col>
-                    <Col xs="auto">
+                    <Col xs="9">
+                        <div className="day_light" style={{background: style}}/>
                         {sun.rising}
                     </Col>
                 </Row>
